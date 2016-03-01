059cad953935271959f0d274f6c3c61eecb3976c
mattck <mattck@localhost.localdomain>
2016-03-01 01:29:52 -0600

    SC

--- util/site.setup.php
+++ util/site.setup.php
@@ -3,8 +3,8 @@
 //define('DOC_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
 const SITE_NAME = '';
-const PATH = '/SmartClerk';
-const PATH_LONG = 'http://localhost'.PATH;
+const PATH = '';
+//const PATH_LONG = 'http://localhost'. PATH;
 const DOC_ROOT = '';
 
 const DB_USER = "User";
@@ -13,18 +13,15 @@ const DB_HOST = 'localhost';
 const DB_PASS = 'Pass';
 
 
-global $dbConn;
+//global $ajaxConn;
+//$ajaxConn = new ajax;
 
-$dbConn = new rdb(DB_HOST, DB_USER, strrev(DB_PASS), DB_NAME);
+define('INCLUDE_JS_JQUERY','<script src="'.PATH.'/scripts/jquery-2.2.0.js"></script>');
+define('INCLUDE_JS_AJAX','<script src="'.PATH.'/scripts/ajax.class.js"></script>');
+define('INCLUDE_JS_MAIN','<script src="'.PATH.'/scripts/main.functions.js"></script>');
 
-global $ajaxConn;
-$ajaxConn = new ajax;
+define('INCLUDE_DEFAULT_JS',INCLUDE_JS_JQUERY."\r\n".INCLUDE_JS_AJAX."\r\n".INCLUDE_JS_MAIN);
 
-const INCLUDE_JS_JQUERY = '<script src="'.PATH.'/scripts/jquery-2.2.0.js"></script>';
-const INCLUDE_JS_AJAX = '<script src="'.PATH.'/scripts/ajax.class.js"></script>';
-const INCLUDE_JS_MAIN = '<script src="'.PATH.'/scripts/main.functions.js"></script>';
+define('INCLUDE_CSS_MAIN ','<link rel="stylesheet" type="text/css" href="'.PATH.'/scripts/main.css">');
+define('INCLUDE_DEFAULT_CSS', '<link rel="stylesheet" type="text/css" href="'.PATH.'/scripts/main.css">');
 
-const INCLUDE_DEFAULT_JS = INCLUDE_JS_JQUERY."\r\n".INCLUDE_JS_AJAX."\r\n".INCLUDE_JS_MAIN;
-
-const INCLUDE_CSS_MAIN = '<link rel="stylesheet" type="text/css" href="'.PATH.'/scripts/main.css">';
-const INCLUDE_DEFAULT_CSS = INCLUDE_CSS_MAIN;
